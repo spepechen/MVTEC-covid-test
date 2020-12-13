67345d4cd610ac447d0c3be9e4dafb011523f945
Nuria Altimir <nuria.altimri@gmail.com>
2020-12-13 14:01:29 +0200

    last update with pivot

--- script/Nuria_timeseriescluster.R
+++ script/Nuria_timeseriescluster.R
@@ -13,21 +13,35 @@ load(file="output/joined_data_by_isocode.RData")
 
 filtered_joined <- joined %>% filter(location != "International" & location != "World" )
 df<-data.frame(filtered_joined)
-selecteddf <- df %>% select(iso_code, date , total_cases_per_million, total_deaths_per_million, stringency_index)
+selecteddf <- df %>% select(iso_code, date , new_cases_smoothed_per_million, stringency_index)%>%
     #selecteddffull<-na_interpolation(selecteddf, option = "linear")  
-drop_na() %>% 
+drop_na() %>%  #drop rows containing na
   glimpse()
 
 
+
+
 #plot the selected data
-ggplot(selecteddf, aes(date, new_cases_per_million, group=iso_code, color=iso_code))+ geom_line()+xlab("")+ theme(legend.position = 'none')
+ggplot(selecteddf, aes(date, new_cases_smoothed_per_million, group=iso_code, color=iso_code))+ geom_line()+xlab("")+ theme(legend.position = 'none')
 
 
-#prepare data for cluster analysis
-selecteddf_spread <- selecteddf %>%
-  spread(iso_code, total_cases_per_million)  
+#Prepare data for cluster analysis
 
-deaths<- t(selecteddf_spread [-1])
+###old version, useful only for 2 variables:
+###   selecteddf_spread <- selecteddf %>%
+###   spread(iso_code, total_cases_per_million) %>%  
+###   deaths<- t(selecteddf_spread [-1])
+
+
+selecteddfpivot <- selecteddf %>%  
+  pivot_wider(id_cols=c('iso_code','new_cases_smoothed_per_million'),
+              names_from='date',
+              names_sep = ".",
+              values_from=c('new_cases_smoothed_per_million', "stringency_index"),
+              values_fill=0) %>%
+  remove_rownames() %>%
+  column_to_rownames("iso_code")
+
 
 #perform the cluster analysis
 #since data here is all numeric, I can calculate 
@@ -36,25 +50,27 @@ deaths<- t(selecteddf_spread [-1])
 #dissimMatrix<-daisy(deaths, metric = "gower", stand=TRUE)
 #distMatrix<-dissimMatrix^2
 
-distMatrix <- dist(deaths, method="euclidean") 
+distMatrix <- dist(selecteddfpivot, method="euclidean") 
 h1 <- hclust(distMatrix,method="ward.D2")  
+
+
+
 
 #Decide the cluster number and join to the database
 
-clustered_data <- cutree(h1, k=11)
-clustered_data_tidy <- as.data.frame(as.table(clustered_data)) %>% glimpse()
+clustered_data <- cutree(h1, k=22)
+clustered_data_tidy <- as.data.frame(as.table(clustered_data)) 
 colnames(clustered_data_tidy) <- c("iso_code","cluster")
 clustered_data_tidy$iso_code <- as.character(clustered_data_tidy$iso_code)
 
 joined_clusters <- selecteddf %>%
-  inner_join(clustered_data_tidy, by = "iso_code") %>%
-  glimpse()
+  inner_join(clustered_data_tidy, by = "iso_code")
 
 
 
 #Line plot according to cluster
 
-ggplot(joined_clusters, aes(date, total_cases_per_million, group=iso_code, color=cluster))+
+ggplot(joined_clusters, aes(date, new_cases_smoothed_per_million, group=iso_code, color=cluster))+
   geom_line()+xlab("")+ 
   theme(legend.position = 'none')+
   facet_wrap(~cluster)
